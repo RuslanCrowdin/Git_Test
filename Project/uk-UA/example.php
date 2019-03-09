ef64e8c3c172defb86f1bf563ca70c880c69af0e
RuslanCrowdin <34035121+RuslanCrowdin@users.noreply.github.com>
2019-03-09 18:04:46 +0200

    New Crowdin translations (#20)
    * New translations Example.ini (Swedish)
    
    * New translations Example.ini (Russian)
    
    * New translations Example.xliff (Russian)
    
    * New translations Example.properties (Japanese)
    
    * New translations Example.properties (Spanish)
    
    * New translations Example.properties (Swedish)
    
    * New translations Example.strings (Japanese)
    
    * New translations Example.strings (Spanish)
    
    * New translations Example.strings (Swedish)
    
    * New translations Example.xliff (Japanese)
    
    * New translations Example.xliff (Spanish)
    
    * New translations Example.po (Swedish)
    
    * New translations Example.xliff (Swedish)
    
    * New translations Example.xliff (Ukrainian)
    
    * New translations Example.yml (Japanese)
    
    * New translations Example.yml (Russian)
    
    * New translations Example.yml (Spanish)
    
    * New translations Example.yml (Swedish)
    
    * New translations Example.yml (Ukrainian)
    
    * New translations example.php (Japanese)
    
    * New translations example.php (Russian)
    
    * New translations example.php (Spanish)
    
    * New translations example.php (Swedish)
    
    * New translations Example.po (Ukrainian)
    
    * New translations Example.po (Spanish)
    
    * New translations Example (Google Chrome Extension).json (Japanese)
    
    * New translations Example (simplest).html (Spanish)
    
    * New translations Example (Google Chrome Extension).json (Russian)
    
    * New translations Example (Google Chrome Extension).json (Spanish)
    
    * New translations Example (Google Chrome Extension).json (Swedish)
    
    * New translations Example (Google Chrome Extension).json (Ukrainian)
    
    * New translations Example.json (Japanese)
    
    * New translations Example.json (Russian)
    
    * New translations Example.json (Spanish)
    
    * New translations Example.json (Swedish)
    
    * New translations Example.json (Ukrainian)
    
    * New translations Example (simplest).html (Japanese)
    
    * New translations Example (simplest).html (Swedish)
    
    * New translations Example.po (Russian)
    
    * New translations Example.ini (Japanese)
    
    * New translations Example.ini (Spanish)
    
    * New translations Example.ini (Ukrainian)
    
    * New translations Example.md (Japanese)
    
    * New translations Example.md (Spanish)
    
    * New translations Example.md (Swedish)
    
    * New translations Example.po (Japanese)
    
    * New translations example.php (Ukrainian)

--- Project/uk-UA/example.php
+++ Project/uk-UA/example.php
@@ -1,10 +1,10 @@
 ﻿<?php
 $en_lang = array (
-"Identifier_1"=>'String for translation 1',
-"Identifier_2"=>'String for translation 2',
-"Identifier_3"=>'String for translation 3',
-"Identifier_4"=>'String for translation 4',
-"Identifier_5"=>'String for translation 5',
+"Identifier_1"=>'',
+"Identifier_2"=>'',
+"Identifier_3"=>'',
+"Identifier_4"=>'',
+"Identifier_5"=>'',
 );
 extract($en_lang);
 ?>
